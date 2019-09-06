65a779a1f07c3d948de6c0b1fbcfbe56adce99d2
jp aulet <blakdrak@gmail.com>
2019-09-06 12:15:15 +0200

    Vuejs app blockstack skeleton

---
+++ src/router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'exact-active',
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    }
+  ]
+})
